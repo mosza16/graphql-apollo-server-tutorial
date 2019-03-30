b77caf1faac8b47ca0d4b02e4bc60b09350b6fc5
kittipong kamnerd <kittipong_mail1@hotmail.com>
2019-03-30 23:09:06 +0700

    add test file

--- src/index.js
+++ src/index.js
@@ -65,10 +65,10 @@ server.applyMiddleware({app, path: '/graphql'});
 const httpServer = http.createServer(app);
 server.installSubscriptionHandlers(httpServer);
 
-const eraseDatabaseOnSync = true;
+const isTest  = process.env.TEST_DATABASE ? true : false;
 
-sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
-    if (eraseDatabaseOnSync) {
+sequelize.sync({ force: isTest }).then(async () => {
+    if (isTest) {
         await createUsersWithMessages(new Date());
     }
 
