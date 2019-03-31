a6eaa1ee6b922e62be8626b97ad0478a5974910c
mosza16 <kittipong.kamnerd@mail.kmutt.ac.th>
2019-03-31 17:37:53 +0700

    update config

--- src/index.js
+++ src/index.js
@@ -34,6 +34,8 @@ const getMe = async req => {
 // const userLoader =  new DataLoader(keys => loaders.user.batchUsers(keys, models));
 
 const server = new ApolloServer({
+    introspection: true,
+    playground: true,
     typeDefs: schema,
     resolvers,
     formatError: error => {
